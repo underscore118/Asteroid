a95721019bd1085e12d4c9f40276483370ed2e6b
underscore118 <vittorio.giordana@protonmail.com>
2025-09-20 01:03:03 +0200

    add_Player

--- asteroid/main.py
+++ asteroid/main.py
@@ -1,5 +1,10 @@
 import pygame
+from player import Player
 from constants import *
+
+
+
+
 
 pygame.init()
 
@@ -8,16 +13,37 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawables)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+
+
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        
 
         screen.fill((0, 0, 0))
+        
+        updatable.update(dt)
+        for sprite in drawables:
+            sprite.draw(screen)
+        
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
  
 
 
